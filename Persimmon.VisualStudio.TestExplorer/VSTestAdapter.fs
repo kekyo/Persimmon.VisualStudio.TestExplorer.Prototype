bd3797625e2205e86b6b32a62b0268ef7dc64d38
pocketberserker <pocketberserker@gmail.com>
2015-10-03 15:30:34 +0900

    wip

--- Persimmon.VisualStudio.TestExplorer/VSTestAdapter.fs
+++ Persimmon.VisualStudio.TestExplorer/VSTestAdapter.fs
@@ -4,21 +4,27 @@ open Microsoft.VisualStudio.TestPlatform.ObjectModel
 open Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter
 open Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging
 open System
+open System.Reflection
 
 [<FileExtension(".dll")>]
 [<ExtensionUri(Constant.extensionUri)>]
 [<DefaultExecutorUri(Constant.extensionUri)>]
 type VSTestAdapter() =
 
+  let version = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+
   let runner = VSTestRunner()
 
   interface ITestDiscoverer with
     member this.DiscoverTests(sources: string seq, context: IDiscoveryContext,logger: IMessageLogger, sink: ITestCaseDiscoverySink) =
       try
-        logger.SendMessage(TestMessageLevel.Informational, sprintf "%A" sources)
-        runner.DiscoverTests(sources, sink)
-      with e ->
-        logger.SendMessage(TestMessageLevel.Error, e.ToString())
+        logger.SendMessage(TestMessageLevel.Informational, sprintf "Persimmon Test Explorer %s discovering tests is started" version)
+        try
+          runner.DiscoverTests(sources, sink)
+        with e ->
+          logger.SendMessage(TestMessageLevel.Error, e.ToString())
+      finally
+        logger.SendMessage(TestMessageLevel.Informational, sprintf "Persimmon Test Explorer %s discovering tests is finished" version)
   interface ITestExecutor with
     member this.RunTests(tests: TestCase seq, runContext: IRunContext, handle: IFrameworkHandle) =
       (this :> ITestExecutor).RunTests(tests |> Seq.map (fun c -> c.Source), runContext, handle)
